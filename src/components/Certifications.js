fa467630d4e5e81f187c3d25a91979bb5cde1238
auschanh <auschanh@gmail.com>
2025-01-11 04:09:51 -0500

    fixed mobile certification responsiveness

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -21,10 +21,10 @@ const Certifications = () => {
                 <div className='text-xl font-extrabold my-4 mb-8 text-black dark:text-zinc-300'>
                   Confluent Certified Administrator for Apache Kafka (December 2024)
                 </div>
-                <div className='flex sm:flex-row items-start'>
-                  <img src={ccaak} className="h-60 sm:h-80" alt="Confluent Certified Administrator for Apache Kafka"></img>
+                <div className='flex flex-col lg:flex-row items-center justify-center lg:items-start'>
+                  <img src={ccaak} className="mb-8 h-60 sm:h-80" alt="Confluent Certified Administrator for Apache Kafka"></img>
                   <div className='sm:ml-6 mt-4 sm:mt-0 dark:text-zinc-300'>
-                    <a className="dark:text-teal-600 text-teal-300 dark:hover:text-gray-600 hover:text-gray-400 hover:underline hover:decoration-rose-600 hover:decoration-2 hover:underline-offset-[6px] font-mono" href="https://certificates.confluent.io/16f1034f-aa22-40c5-ad14-db3a7cbea5bb#acc.7Mcymg3r" target="_blank"
+                    <a className="font-bold dark:text-teal-600 text-teal-300 dark:hover:text-gray-600 hover:text-gray-400 hover:underline hover:decoration-rose-600 hover:decoration-2 hover:underline-offset-[6px] font-mono" href="https://certificates.confluent.io/16f1034f-aa22-40c5-ad14-db3a7cbea5bb#acc.7Mcymg3r" target="_blank"
                 rel="noopener noreferrer">View Certification Page</a>
                     <br /><br/>
                     <p>Obtained CCAAK, showcasing administrating abilities with Confluent and Apache Kafka. Demonstrates an understanding of:</p>
