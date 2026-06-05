ed6230dba556fc51345b437466e2e108c1f0b578
auschanh <auschanh@gmail.com>
2026-06-05 03:21:06 -0400

    added new photos

--- src/data/photographyData.js
+++ src/data/photographyData.js
@@ -1,65 +1,95 @@
 import pic4  from '../assets/photography/20191229-DSC_0564.jpg';
-import pic5  from '../assets/photography/20200104-DSC_0308.jpg';
 import pic6  from '../assets/photography/20200102-DSC_0885.jpg';
 import pic7  from '../assets/photography/20200102-DSC_0937.jpg';
 import pic8  from '../assets/photography/20200102-DSC_0028.jpg';
 import pic9  from '../assets/photography/20200102-DSC_0986.jpg';
 import pic10 from '../assets/photography/20200104-DSC_0272.jpg';
 import pic13 from '../assets/photography/MarlieGasStation2.jpg';
-import pic15 from '../assets/photography/DSC_0378.jpg';
 import pic16 from '../assets/photography/2025/20250727-DSCF0257.jpg';
-import pic17 from '../assets/photography/2025/20250727-DSCF0269.jpg';
-import pic18 from '../assets/photography/2025/20250727-DSCF0274.jpg';
-import pic19 from '../assets/photography/2025/20250803-DSCF0399.jpg';
 import pic20 from '../assets/photography/2025/20250803-DSCF0470.jpg';
 import pic21 from '../assets/photography/2025/20250808-DSCF0500.jpg';
-import pic22 from '../assets/photography/2025/20250808-DSCF0502.jpg';
-import pic23 from '../assets/photography/2025/20250808-DSCF0547.jpg';
 import pic24 from '../assets/photography/2025/20250808-DSCF0562.jpg';
-import pic25 from '../assets/photography/2025/20250809-DSCF0623.jpg';
 import pic26 from '../assets/photography/2025/20250809-DSCF0643.jpg';
 import pic27 from '../assets/photography/2025/20250809-DSCF0673.jpg';
 import pic28 from '../assets/photography/2025/20250809-DSCF0698.jpg';
-import pic29 from '../assets/photography/2025/20250809-DSCF0706.jpg';
 import pic30 from '../assets/photography/2025/20250809-DSCF0711.jpg';
-import pic31 from '../assets/photography/2025/20250701-DSCF0150.jpg';
-import pic32 from '../assets/photography/2025/20250701-DSCF0154.jpg';
 import pic34 from '../assets/photography/2025/20250701-DSCF0218.jpg';
 import pic35 from '../assets/photography/2025/20250701-DSCF0220.jpg';
 import pic36 from '../assets/photography/2025/20250701-DSCF0224.jpg';
+import pic37 from '../assets/photography/2025-Dec/20251130-DSCF2679.jpg';
+import pic38 from '../assets/photography/2025-Dec/20251130-DSCF2690.jpg';
+import pic39 from '../assets/photography/2025-Dec/20251130-DSCF2719.jpg';
+import pic40 from '../assets/photography/2025-Dec/20251130-DSCF2733.jpg';
+import pic41 from '../assets/photography/2025-Dec/DSCF2555.JPG';
+import pic42 from '../assets/photography/2025-Dec/DSCF2608.JPG';
+import pic43 from '../assets/photography/2025-Dec/DSCF2933.JPG';
+import pic44 from '../assets/photography/2025-Dec/DSCF2968.JPG';
+import pic45 from '../assets/photography/2025-Dec/DSCF2980.JPG';
+import pic46 from '../assets/photography/2025-Dec/DSCF3010.JPG';
+import pic47 from '../assets/photography/2025-Dec/DSCF3021.JPG';
+import pic48 from '../assets/photography/2025-Dec/DSCF3043.JPG';
+import pic49 from '../assets/photography/2025-Dec/DSCF3064.JPG';
+import pic50 from '../assets/photography/2025-Dec/DSCF3071.JPG';
+import pic51 from '../assets/photography/2025-Dec/DSCF3091.JPG';
+import pic52 from '../assets/photography/2025-Dec/DSCF3115.JPG';
+import pic53 from '../assets/photography/2025-Dec/DSCF3123.JPG';
+import pic54 from '../assets/photography/2025-Dec/DSCF3194.JPG';
+import pic55 from '../assets/photography/2025-Dec/DSCF3428.JPG';
+import pic56 from '../assets/photography/2025-Dec/DSCF3442.JPG';
+import pic57 from '../assets/photography/2025-Dec/DSCF3457.JPG';
+import pic58 from '../assets/photography/2025-Dec/DSCF3466.JPG';
+import pic59 from '../assets/photography/2025-Dec/DSCF3661.JPG';
+import pic60 from '../assets/photography/2025-Dec/DSCF3662.JPG';
+import pic61 from '../assets/photography/2025-Dec/DSCF3707.JPG';
+import pic62 from '../assets/photography/2025-Dec/DSCF3711.JPG';
+import pic63 from '../assets/photography/2025-Dec/DSCF4018.JPG';
 
 // To add a photo:  1) import it above  2) add an entry to the array below
 // To remove:       comment out or delete the import and the array entry
 // Order in the array = order displayed in the masonry grid
 
 export const photos = [
+  { src: pic37, title: 'Vancouver, British Columbia 2025 — Granville Island',   alt: 'Boat in water in Vancouver' },
+  { src: pic38, title: 'Vancouver, British Columbia 2025 — Granville Island',   alt: 'Boat in water in Vancouver with skyline view' },
+  { src: pic39, title: 'Vancouver, British Columbia 2025 — Granville Island',   alt: 'Close up of man smoking' },
+  { src: pic40, title: 'Vancouver, British Columbia 2025 — Granville Island',   alt: 'Upward shot of Vancouver buildings' },
+  { src: pic41, title: 'Toronto, Ontario 2025 — Distillery District',   alt: 'Upward shot of lights in Distillery District' },
+  { src: pic42, title: 'Vancouver, British Columbia 2025 — Downtown',   alt: 'Skyline view of downtown Vancouver with mountains in background' },
+  { src: pic43, title: 'Vancouver, British Columbia 2025 — Stanley Park',   alt: 'Photo of small lighthouse near Stanley Park' },
+  { src: pic44, title: 'Vancouver, British Columbia 2025 — North Vancouver',   alt: 'Photo of houses in North Vancouver' },
+  { src: pic45, title: 'Vancouver, British Columbia 2025 — Squamish',   alt: 'Man looking out window in sea to sky gondola in Squamish' },
+  { src: pic46, title: 'Vancouver, British Columbia 2025 — Squamish',   alt: 'Man smiling at a held camera in sea to sky gondola in Squamish' },
+  { src: pic47, title: 'Vancouver, British Columbia 2025 — Mount Habrich',   alt: 'Two men smiling with mountain view on top Mount Habrich ' },
+  { src: pic48, title: 'Vancouver, British Columbia 2025 — Mount Habrich',   alt: 'Two men looking out to mountains with during sunset on Mount Habrich' },
+  { src: pic49, title: 'Vancouver, British Columbia 2025 — Mount Habrich',   alt: 'Man pointing to sky on bridge at Mount Habrich' },
+  { src: pic50, title: 'Vancouver, British Columbia 2025 — Mount Habrich',   alt: 'View of lake and trees on top of Mount Habrich' },
+  { src: pic51, title: 'Vancouver, British Columbia 2025 — Mount Habrich',   alt: 'Photo of trees and mountain on hiking trail on Mount Habrich' },
+  { src: pic52, title: 'Vancouver, British Columbia 2025 — Mount Habrich',   alt: 'Photo of trees and mountain on hiking trail on Mount Habrich' },
+  { src: pic53, title: 'Vancouver, British Columbia 2025 — Mount Habrich',   alt: 'Photo of Oleson Falls lookout sign' },
+  { src: pic54, title: 'Vancouver, British Columbia 2025 — Whistler',   alt: 'Black and white photo of 3 geese in water' },
+  { src: pic56, title: 'St Catherines, Ontario 2025',   alt: 'Man plating his cooking with sauce' },
+  { src: pic57, title: 'Burlington, Ontario 2025',   alt: 'Upward shot of Burlington house' },
+  { src: pic58, title: 'Burlington, Ontario 2025',   alt: 'Black and white photo of shed' },
+  { src: pic59, title: 'Milton, Ontario 2025',   alt: 'Black and white photo of elderly asian woman' },
+  { src: pic61, title: 'Niagara Falls, Ontario 2025',   alt: 'Photo of baby staring into camera' },
+  { src: pic62, title: 'Niagara Falls, Ontario 2025',   alt: 'Photo of young girl making a peace sign to camera' },
+  { src: pic63, title: 'Port Credit, Ontario 2026',   alt: 'Close up photo of cherry blossom branch' },
   { src: pic16, title: 'Toronto, Ontario 2025 — High Park',                    alt: 'Dog sitting in shade at High Park' },
-  { src: pic17, title: 'Niagara Falls, Ontario 2025 — Queenston Heights Park', alt: 'Baby boy eating chips and smiling at camera' },
-  { src: pic18, title: 'Niagara Falls, Ontario 2025 — Queenston Heights Park', alt: 'Little girl squatting in shade looking to the side' },
-  { src: pic19, title: 'Milton, Ontario 2025 — Kelso Conservation Area',       alt: 'Baby girl and mother smiling at camera' },
   { src: pic20, title: 'Milton, Ontario 2025 — Kelso Conservation Area',       alt: 'Group of adult men at a beach laughing' },
   { src: pic21, title: 'Montreal, Quebec 2025 — Palais des congrès',           alt: 'Rooftop picture in sunset of Montreal flag' },
-  { src: pic22, title: 'Montreal, Quebec 2025 — Palais des congrès',           alt: 'Rooftop picture in sunset of Montreal buildings' },
-  { src: pic23, title: 'Montreal, Quebec 2025 — Old Port of Montreal',         alt: 'Night photo of well lit streets and cars' },
   { src: pic24, title: 'Montreal, Quebec 2025 — Old Port of Montreal',         alt: 'Night photo of Old Port ferris wheel with water reflection' },
-  { src: pic25, title: 'Montreal, Quebec 2025 — Old Montreal',                 alt: 'Night photo of Old Montreal streets — man sitting outside café' },
   { src: pic26, title: 'Montreal, Quebec 2025 — Old Montreal',                 alt: 'Day shot of buildings and outdoor dining tables' },
   { src: pic27, title: 'Montreal, Quebec 2025 — Anime Convention',             alt: 'Cosplayers dressed as Kishibe and Quanxi from Chainsaw Man' },
   { src: pic28, title: 'Montreal, Quebec 2025 — Mount Royal',                  alt: 'Sunset shot of Montreal skyline' },
-  { src: pic29, title: 'Montreal, Quebec 2025 — Mount Royal',                  alt: 'Sunset shot of Montreal skyline' },
   { src: pic30, title: 'Montreal, Quebec 2025 — Mount Royal',                  alt: 'Person smiling at Mount Royal skyline view' },
-  { src: pic31, title: 'Toronto, Ontario 2025 — Mimico Park',                  alt: 'Boat in water around Mimico Park' },
-  { src: pic32, title: 'Toronto, Ontario 2025 — Mimico Park',                  alt: 'Bike resting on rocks near lake' },
   { src: pic34, title: 'Toronto, Ontario 2025 — High Park',                    alt: 'Man sitting in chair near lake' },
   { src: pic35, title: 'Toronto, Ontario 2025 — High Park',                    alt: 'Man adjusting fishing rod near High Park river' },
   { src: pic36, title: 'Toronto, Ontario 2025 — High Park',                    alt: 'High Park river with buildings in background' },
   { src: pic4,  title: 'Fujikawaguchiko, Japan 2019',                          alt: 'Small road with car in the town of Fujikawaguchiko' },
-  { src: pic5,  title: 'Kyoto, Japan 2020 — Fushimi Inari Shrine',             alt: 'Row of Fushimi Inari torii gates' },
   { src: pic6,  title: 'Hiroshima, Japan 2020 — Miyajima Island',              alt: 'View of Miyajima Island from boat' },
   { src: pic7,  title: 'Hiroshima, Japan 2020 — Miyajima Island',              alt: 'Child and deer posing at sunset' },
   { src: pic8,  title: 'Hiroshima, Japan 2020 — Miyajima Island',              alt: 'Close up of nara deer' },
   { src: pic9,  title: 'Hiroshima, Japan 2020 — Miyajima Island',              alt: 'Close up of Japanese origami decoration' },
   { src: pic10, title: 'Kyoto, Japan 2020 — Shimogyō-ku',                     alt: 'Women in kimonos walking on a Kyoto street' },
   { src: pic13, title: 'Joshua Tree, California 2022 — Route 66',              alt: 'Girl posing by retro car at desert gas station', credit: 'Justin Wong' },
-  { src: pic15, title: 'Toronto, Ontario 2019 — High Park',                    alt: 'Cherry blossom branches with sun in background' },
 ];
